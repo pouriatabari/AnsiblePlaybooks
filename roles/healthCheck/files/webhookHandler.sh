0fd6aad82a61d16f2561534f9325779df176015e
Pourya Tabari <p.tabari@asax.local>
2024-12-25 19:08:40 +0330

    handle error at creating exist container

--- roles/healthCheck/files/webhookHandler.sh
+++ roles/healthCheck/files/webhookHandler.sh
@@ -9,8 +9,13 @@ nc -l -p $webhook_port -q 1 | while read line; do
 
   # Check Alert and Replace Damaged Containers
   if echo "$line" | grep -q "server"; then
-    echo "Replacing container..."
-    docker run -d --name webserver_replacement -p 80:80 nginx
-    echo "Replacement container started."
+    # Check if the replacement container is already running
+    if ! docker ps -q -f name=webserver_replacement; then
+      echo "Replacing container..."
+      docker run -d --name webserver_replacement -p 80:80 nginx
+      echo "Replacement container started."
+    else
+      echo "Container already exists, skipping replacement."
+    fi
   fi
 done
