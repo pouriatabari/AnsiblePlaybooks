f46688265e33d366541d16daf60fe5aa4da12de5
Pourya Tabari <p.tabari@asax.local>
2024-12-25 17:05:46 +0330

    resolve issue to use service file

--- roles/healthCheck/files/healthCheck.sh
+++ roles/healthCheck/files/healthCheck.sh
@@ -3,23 +3,19 @@
 # Web Server Address List
 webservers=("http://192.168.72.20" "http://192.168.72.21" "http://192.168.72.22")
 
-# Push Gateway Address List
+# Push Gateway Address
 pushgateway_url="http://192.168.72.23:9091/metrics/job/webserver_health"
 
-while true; do
-  for server in "${webservers[@]}"; do
-    # Web Server Health check
-    if curl -s --head --request GET "$server" | grep "200 OK" > /dev/null; then
-      status=1
-    else
-      status=0
-    fi
+for server in "${webservers[@]}"; do
+  # Web Server Health check
+  if curl -s --head --request GET "$server" | grep "200 OK" > /dev/null; then
+    status=1
+  else
+    status=0
+  fi
 
-    # Sending Metrics to the Push Gateway
-    metric_data="webserver_health{server=\"$server\"} $status"
-    echo "$metric_data" | curl -X POST --data-binary @- "$pushgateway_url"
-    echo "Health status of $server: $status"
-  done
-  # Running Script every 60s
-  sleep 60
+  # Sending Metrics to the Push Gateway
+  metric_data="webserver_health{server=\"$server\"} $status"
+  echo "$metric_data" | curl -X POST --data-binary @- "$pushgateway_url"
+  echo "Health status of $server: $status"
 done
--- roles/healthCheck/files/webhookHandler.sh
+++ roles/healthCheck/files/webhookHandler.sh
@@ -4,13 +4,15 @@
 webhook_port=5000
 
 # Create simple Server to receive Webhook
-nc -l -p $webhook_port | while read line; do
-  echo "Received alert: $line"
+response=$(timeout 1 nc -l -p $webhook_port)
 
-  # Check Alart and Rerplace Damaged Containers
-  if echo "$line" | grep -q "server"; then
+if [ -n "$response" ]; then
+  echo "Received alert: $response"
+
+  # Check Alert and Replace Damaged Containers
+  if echo "$response" | grep -q "server"; then
     echo "Replacing container..."
     docker run -d --name webserver_replacement -p 80:80 nginx
     echo "Replacement container started."
   fi
-done
+fi
